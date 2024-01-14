f4ca7b881284633522e123b8becca98f86558c40
Alan <alancvsnowden@hotmail.co.uk>
2024-01-14 20:59:44 +0000

    Added questions to inquirer function

--- utils/index.js
+++ utils/index.js
@@ -14,8 +14,57 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'Project title',
+                message: 'What is the title of your project?',
+            },
+            {
+                type: 'input',
+                name: 'Description',
+                message: 'Please give a description of your project:',
+            },
+            {
+                type: 'input',
+                name: 'Table of contents',
+                message: 'Please list out the sections you want to include in your table of contents:',
+            },
+            {
+                type: 'input',
+                name: 'Installation',
+                message: 'How would a user install this application?',
+            },
+            {
+                type: 'input',
+                name: 'Usage',
+                message: 'How do you use this application?',
+            },
+            {
+                type: 'input',
+                name: 'License',
+                message: 'Please select one of the following licenses for this application:',
+            },
+            {
+                type: 'input',
+                name: 'Contribution',
+                message: 'List any resources used in this application eligable for recognition',
+            },
+            {
+                type: 'input',
+                name: 'Tests',
+                message: 'How should the user test this application?',
+            },
+            {
+                type: 'input',
+                name: 'Questions',
+                message: 'Enter your email address, so users can contact you with questions:',
+            },
+        ])};
+        .then((answers) => {
+            generateMarkdown(answers);
+        });
 
-}
-
-// function call to initialize program
-init();
+    // function call to initialize program
+    init();
